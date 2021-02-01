b604e673be30b3aad96b18d2046cbd68d965e448
viktoriakomarovskaya <viktoriia.komarovska@sixt.com>
2021-02-01 22:35:59 +0200

    enhance animations and fancy font

--- src/components/AnimationCircle.js
+++ src/components/AnimationCircle.js
@@ -5,13 +5,26 @@ import "../styles/index.sass";
 const AnimationCircle = () => {
     return (
         <div className="animation-container">
-            <div className="circle outer-circle"></div>
+            <div className="circle outer-circle black">
             <div className="animation-circle-orbit outer">
                 <div className="dot dot-outer" />
             </div>
-            <div className="circle inner-circle"></div>
-            <div className="animation-circle-orbit inner">
-                <div className="dot dot-inner" />
+            </div>
+            {/*<div className="circle inner-circle black">*/}
+            {/*<div className="animation-circle-orbit inner">*/}
+            {/*    <div className="dot dot-inner" />*/}
+            {/*</div>*/}
+            {/*</div>*/}
+            <div className="circle ellipse"></div>
+            <div className="headline-container">
+                <div className="headline">
+                    Viktoriia Komarovska, frontend developer
+                </div>
+            </div>
+            <div className="viewport">
+               <span className="moveX">
+                   <div className="moveY"><span className="elipsoid"></span></div>
+               </span>
             </div>
             {/*<div className="deform">*/}
             {/*    <div className="rotate">*/}
